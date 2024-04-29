1e6337c6badf6aaa2933b248b640fb40bd4ad5b4
aparajit0511 <aparajit35@gmail.com>
2024-04-29 19:29:46 +0530

    second commit

--- src/main/java/com/example/javapractice/TODOList/controller/TodoController.java
+++ src/main/java/com/example/javapractice/TODOList/controller/TodoController.java
@@ -1,9 +1,14 @@
 package com.example.javapractice.TODOList.controller;
 
 
+import com.example.javapractice.TODOList.entity.Todo;
 import com.example.javapractice.TODOList.service.TodoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/todos")
@@ -12,5 +17,17 @@ public class TodoController {
     private TodoService todoService;
     public TodoController(TodoService theTodoService){
         todoService = theTodoService;
+    }
+
+    @GetMapping("/list")
+    public String listTodos(Model theModel){
+
+        // get todos from the db
+        List<Todo> todos = todoService.findAll();
+
+        // add that to the spring model
+        theModel.addAttribute("todos",todos);
+
+        return "todos/list-todos";
     }
 }
